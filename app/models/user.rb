98300e0c91f09e61c122f92df9826f85870fdc56
FyisFe <yfeng28-c@my.cityu.edu.hk>
2020-04-11 18:47:08 +0800

    update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,23 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  attr_accessor :password, :password_confirmation
+
+  validates_presence_of :email, message: "Enter a valid email address!"
+  validates :email, uniqueness: true
+
+  validates_presence_of :password, message: "Enter your password！", 
+    if: :need_validate_password
+  validates_presence_of :password_confirmation, message: "Enter your password！",
+    if: :need_validate_password
+  validates_confirmation_of :password, message: "Passwords must match",
+    if: :need_validate_password
+  validates_length_of :password, message: "Passwords must be at least 6 characters！", minimum: 6,
+    if: :need_validate_password
+
+  private
+  def need_validate_password
+    self.new_record? || 
+      (!self.password.nil? || !self.password_confirmation.nil?)
+  end
 end
