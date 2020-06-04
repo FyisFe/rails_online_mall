965d7c40acadf7a46e6859edc67bc07f93df2633
FyisFe <yfeng28-c@my.cityu.edu.hk>
2020-06-04 16:45:23 +0800

    add function of creating new categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,7 +1,25 @@
 class Admin::CategoriesController < Admin::BaseController
+  
   def index
+    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10)
+      .order(id: "desc")
   end
 
   def new
+    @category = Category.new
+    @root_categories = Category.roots.order(id: "desc")
   end
+
+  def create
+    @category = Category.new(params.require(:category).permit!)
+    @root_categories = Category.roots.order(id: "desc")
+    if @category.save
+      flash[:notice] = "Saved Successfully!"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+
+
 end
