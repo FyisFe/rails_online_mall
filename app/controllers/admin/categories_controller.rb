5627c43d2e2009eeb009c299b277ec426d2b1b6e
FyisFe <yfeng28-c@my.cityu.edu.hk>
2020-06-06 16:11:54 +0800

    add edit and delete function for product management

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,18 +1,27 @@
 class Admin::CategoriesController < Admin::BaseController
+
+  before_action :find_root_categories, only: [:new, :create, :edit, :update]
+  before_action :find_category, only: [:edit, :update, :destroy]
+  
   
   def index
-    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10)
+    if params[:id].blank?
+      @categories = Category.roots
+    else
+      @category = Category.find(params[:id])
+      @categories = @category.children
+    end
+
+    @categories = @categories.page(params[:page] || 1).per_page(params[:per_page] || 10)
       .order(id: "desc")
   end
 
   def new
     @category = Category.new
-    @root_categories = Category.roots.order(id: "desc")
   end
 
   def create
     @category = Category.new(params.require(:category).permit!)
-    @root_categories = Category.roots.order(id: "desc")
     if @category.save
       flash[:notice] = "Saved Successfully!"
       redirect_to admin_categories_path
@@ -21,5 +30,41 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+
+  def edit
+    render action: :new
+  end
+
+  def update
+    @category.attributes=params.require(:category).permit!
+
+    if @category.save 
+      flash[:notice] = 'Update finshed'
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+  
+
+  def destroy
+    if @category.destroy
+      flash[:notice] = 'Deletion successes'
+      redirect_to admin_categories_path
+    else
+      flash[:notice] = 'Fail to delete'
+      redirect_to :back
+    end
+  end
+
+
+  private
+  def find_root_categories
+    @root_categories = Category.roots.order(id: "desc")
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 
 end
